acb3594f43ed153af82966201951dd0e3d08742f
AndSim <siminiucandrew@gmail.com>
2023-07-04 20:03:40 +0300

    feat: calculate summary and display: in, out, interest

--- script.js
+++ script.js
@@ -78,3 +78,24 @@ const calcDisplayBalance = function (movements) {
     labelBalance.textContent = `${balance} €`;
 };
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+    const incomes = movements
+        .filter((mov) => mov > 0)
+        .reduce((acc, mov) => acc + mov, 0);
+    labelSumIn.textContent = `${incomes} €`;
+
+    const out = movements
+        .filter((mov) => mov < 0)
+        .reduce((acc, mov) => acc + mov, 0);
+    labelSumOut.textContent = `${Math.abs(out)}€`;
+    const interest = movements
+        .filter((mov) => mov > 0)
+        .map((deposit) => (deposit * 1.2) / 100)
+        .filter((int, i, arr) => {
+            return int >= 1;
+        })
+        .reduce((acc, int) => acc + int, 0);
+    labelSumInterest.textContent = `${interest} €`;
+};
+calcDisplaySummary(account1.movements);
