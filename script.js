58a18598ffa96db9b5e4d23a3b6e72ecd33c4487
AndSim <siminiucandrew@gmail.com>
2023-07-25 21:53:29 +0300

    feat: implement the timer to simulate the approval of a loan

--- script.js
+++ script.js
@@ -217,11 +217,13 @@ btnLoan.addEventListener('click', function (e) {
     const amount = Math.floor(inputLoanAmount.value);
 
     if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-        currentAccount.movements.push(amount);
+        setTimeout(function () {
+            currentAccount.movements.push(amount);
 
-        currentAccount.movementsDates.push(new Date().toISOString());
+            currentAccount.movementsDates.push(new Date().toISOString());
 
-        updateUI(currentAccount);
+            updateUI(currentAccount);
+        }, 2500);
     }
     inputLoanAmount.value = '';
 });
